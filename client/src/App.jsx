c89b1b774c5478fff447b800c9b991e5651da5e0
Tania <tanner.shirkey@gmail.com>
2024-07-11 16:48:19 -0400

    fully implemented index files
    individual pages and components can now be accessed from their respective folders in one line

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 // import { Outlet } from 'react-router-dom';
 import "./App.css";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import { Header, } from "./components";
+import { Home } from "./pages";
 
 function App() {
   return (
