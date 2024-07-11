e7edcebf6b011b6cf631329de230d435cbe49cb0
Tania <tanner.shirkey@gmail.com>
2024-07-11 16:35:17 -0400

    substitute page files created
    created temporary files to develop each page separately

---
+++ client/src/pages/Budgets.jsx
@@ -0,0 +1,8 @@
+// Stand-in for full Budget viewing page
+export default function Budget() {
+  return (
+    <main>
+      <h1>To Be Developed!</h1>
+    </main>
+  );
+}
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+// Stand-in for full Account creation page
+export default function Signup() {
+  return (
+    <main>
+      <h1>To Be Developed!</h1>
+    </main>
+  );
+}
