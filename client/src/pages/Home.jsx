f641a6488c42b9d11f4588e736cba4859b519cb8
Tania <tanner.shirkey@gmail.com>
2024-07-13 16:21:59 -0400

    added modal
    added a modal to the main homepage, to be developed to be the main way for users to add new expenses

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,24 @@
-import { useState } from "react";
+// import ReactDOM from "react-dom";
+import { useState, useId } from "react";
 import formatCost from "../utils/formatCost";
 import ExpenseCard from "./ExpenseCard";
+import Modal from "react-modal";
+
+// set the modal element using its id
+Modal.setAppElement("#root");
 
 export default function Home() {
+  const descriptionId = useId();
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   let budgetAmount = 100;
   // Placeholder expenses list, to be replaced with db connection later on
   const [expenses] = useState([
@@ -25,7 +41,35 @@ export default function Home() {
       <div className="budget">
         <h2 className="budget-title">Budget:</h2>
         <h3 className="budget-amount">{formatCost.format(budgetAmount)}</h3>
-        
+      </div>
+
+      <button onClick={openModal} id="add-expense-button">Add Expense</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Add Expense">
+        <button onClick={closeModal}>exit</button>
+        <form id="expense-form">
+          <label htmlFor="titleInput">Expense Title:</label>
+          <br />
+          <input name="titleInput" type="text" required />
+          <br />
+
+          <label htmlFor={descriptionId}>Description:</label>
+          <br />
+          <textarea
+            id={descriptionId}
+            name="descriptionInput"
+            rows={4}
+            cols={40}
+            required
+          />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </Modal>
+
+      <div className="expense-list">
         {expenses.map((expense, index) => (
           <ExpenseCard expense={expense} key={"expense" + index} />
         ))}
@@ -33,3 +77,5 @@ export default function Home() {
     </>
   );
 }
+
+// ReactDOM.createRoot(<Home />, appElement);
