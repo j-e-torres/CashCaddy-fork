dc24041bfff40f00d778fdd9961ec82bcabeba01
Tania <tanner.shirkey@gmail.com>
2024-07-13 19:02:09 -0400

    added expense creation
    users may now create new expenses to keep track of. Bare functionality done, currently doesn't check against the total budget but does effectively extract and use user defined information.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,7 +8,28 @@ import Modal from "react-modal";
 Modal.setAppElement("#root");
 
 export default function Home() {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    // destructure arrays out of formData object so we can check them
+    const [ title, amount, description, categories ] = formData;
+    
+    const newExpense = {
+      title: title[1],
+      description: description[1],
+      amount: parseInt(amount[1]),
+      categories: categories[1].trim().split(", "), // split categories by ", " so that users can save multiple tags
+    }
+
+    setExpenses([...expenses, newExpense]);
+    closeModal();
+  }
+
   const descriptionId = useId();
+
   const [modalIsOpen, setIsOpen] = useState(false);
 
   function openModal() {
@@ -21,7 +42,7 @@ export default function Home() {
 
   let budgetAmount = 100;
   // Placeholder expenses list, to be replaced with db connection later on
-  const [expenses, setExpense] = useState([
+  const [expenses, setExpenses] = useState([
     {
       title: "Ice-Cream",
       description: "bought tasty Ice-Cream :p",
@@ -46,33 +67,36 @@ export default function Home() {
       <button onClick={openModal} id="add-expense-button">
         Add Expense
       </button>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Add Expense" className="animate-top">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Add Expense"
+        className="animate-top">
         <button className="modal-close" onClick={closeModal}>
           &times;
         </button>
         <div className="modal-content">
-          <form id="expense-form" className="modal-content">
+          <form id="expense-form" className="modal-content" onSubmit={handleSubmit}>
             <label htmlFor="titleInput">Expense Title:</label>
             <br />
             <input name="titleInput" type="text" required />
             <br />
 
-            <label htmlFor="amountInput">Amount:</label><br />
-            <input name="amountInput" type="number" required /><br />
+            <label htmlFor="amountInput">Amount:</label>
+            <br />
+            <input name="amountInput" type="number" required />
+            <br />
 
             <label htmlFor={descriptionId}>Description:</label>
             <br />
-            <textarea
-              id={descriptionId}
-              name="descriptionInput"
-              rows={4}
-              cols={40}
-            />
+            <textarea id={descriptionId} name="descriptionInput" rows={4} cols={40} />
             <br />
 
             {/* Currently, category input will have to be a stand-in for something more complicated later on */}
-            <label htmlFor="categoryInput">Categories:</label><br />
-            <input name="categoryInput" type="text" /><br />
+            <label htmlFor="categoryInput">Categories:</label>
+            <br />
+            <input name="categoryInput" type="text" />
+            <br />
 
             <input type="submit" value="Submit" />
           </form>
