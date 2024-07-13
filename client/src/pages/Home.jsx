b0e52da3d87a03027767abf1d4d24d7286a8d923
Tania <tanner.shirkey@gmail.com>
2024-07-13 17:22:16 -0400

    small css touches
    added some small touches to the css of the modal just so it looks a bit nicer; back to work

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -43,30 +43,33 @@ export default function Home() {
         <h3 className="budget-amount">{formatCost.format(budgetAmount)}</h3>
       </div>
 
-      <button onClick={openModal} id="add-expense-button">Add Expense</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Add Expense">
-        <button onClick={closeModal}>exit</button>
-        <form id="expense-form">
-          <label htmlFor="titleInput">Expense Title:</label>
-          <br />
-          <input name="titleInput" type="text" required />
-          <br />
+      <button onClick={openModal} id="add-expense-button">
+        Add Expense
+      </button>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Add Expense" className="animate-top">
+        <button className="modal-close" onClick={closeModal}>
+          &times;
+        </button>
+        <div className="modal-content">
+          <form id="expense-form" className="modal-content">
+            <label htmlFor="titleInput">Expense Title:</label>
+            <br />
+            <input name="titleInput" type="text" required />
+            <br />
 
-          <label htmlFor={descriptionId}>Description:</label>
-          <br />
-          <textarea
-            id={descriptionId}
-            name="descriptionInput"
-            rows={4}
-            cols={40}
-            required
-          />
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
+            <label htmlFor={descriptionId}>Description:</label>
+            <br />
+            <textarea
+              id={descriptionId}
+              name="descriptionInput"
+              rows={4}
+              cols={40}
+              required
+            />
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </Modal>
 
       <div className="expense-list">
