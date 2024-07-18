c694fab1215211148ba2fa5ec22053ac5d1aaf53
Tania <tanner.shirkey@gmail.com>
2024-07-18 18:56:49 -0400

    changed react rulings

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'react/prop-types': 'off',
     'no-unused-vars': 'off',
+    'react/no-unescaped-entities': 'off',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
